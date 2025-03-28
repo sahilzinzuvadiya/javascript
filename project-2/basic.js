933ee33d3bd175e10b54425be8e487e1544671db
sahilzinzuvadiya <sahilzinzuvadiya77@gmail.com>
2025-03-28 12:09:44 +0530

    project-2

---
+++ project-2/basic.js
@@ -0,0 +1,15 @@
+function show()
+{
+    let a=document.getElementById("menubtn");
+    a.classList.toggle("hide");
+    
+    
+    let b=document.getElementById("menu");
+    b.classList.toggle("menu");
+
+    let c=document.getElementById("close");
+    c.classList.toggle("close");
+}
+function show1(){
+    
+}
